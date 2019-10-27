215e9938ef3f17b7917265edf9805cdd8c550bd2
Sky Mendes <skymendesross@gmail.com>
2019-10-27 14:20:32 -0500

    finished

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -1,5 +1,14 @@
 'use strict';
 
+//input a word
+// check if a letter is a vowel using a for loop
+// store index of first vowel
+// removing everything in the indexes < the vowel
+// append removed string to the end of the string
+// additionally append 'ay' to the end of the string
+// edge cases*
+// words with no vowel just gets 'ay' appended
+
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -9,15 +18,43 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
+  word = word.toLowerCase().trim();
+  const vowel = ['a', 'e', 'i', 'o', 'u', 'y'];
+  let storeIndex = 0;
 
-  // Your code here
+  // passed test in 29ms
+  
+  // for (let i=0; i<word.length; i++) {
+  //   for (let j=0; j<vowel.length; j++) {
+  //     if( word[i] === vowel[j]){
+  //       storeIndex = i;
+  //       const str1 = word.slice(0,storeIndex);
+  //       const str2 = word.slice(storeIndex);
+  //       if(storeIndex === 0){
+  //         return str2 + str1 + 'yay';
+  //       }
+  //       return str2 + str1 + 'ay';
+  //     }
+  //   }
+  // }
+
+  for (let i = 0; i < word.length; i++) {
+    if (vowel.includes(word[i])) {
+      if (i === 0) {
+        return word + 'yay';
+      }
+      const str1 = word.slice(0, i);
+      const str2 = word.slice(i);
+      return str2 + str1 + 'ay';
+    }
+  }
 
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
